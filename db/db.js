6b302762034f467ff560e326cf161f23676e4bfb
*Vaibhav <*vaibhavjagtap0925@gmail.com*>
2026-01-26 10:39:36 +0530

    Fix case-sensitive controller path

--- db/db.js
+++ db/db.js
@@ -1,3 +1,24 @@
+// import mysql from "mysql2";
+// import dotenv from "dotenv";
+// dotenv.config();   
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.log()
+//     console.error("❌ MySQL connection failed:", err.message);
+//     return;
+//   }
+//   console.log("✅ MySQL Connected");
+// });
+
+// export default db;
+
 import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();   
--- routes/teacherRoute.js
+++ routes/teacherRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createTeacher, getTeachers } from "../Controllers/teacherController.js";
+import { createTeacher, getTeachers } from "../controllers/teacherController";
 
 const router = express.Router();
 /**
