8870f3fe7fd12d4fcf60bc59b882afa7766d41f9
*Vaibhav <*vaibhavjagtap0925@gmail.com*>
2026-01-30 09:50:47 +0530

    new coonection add

--- db/db.js
+++ db/db.js
@@ -8,13 +8,13 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-// ✅ Test connection using query
+// ✅ Test connection
 db.query("SELECT 1", (err) => {
   if (err) {
     console.error("❌ MySQL connection failed:", err.message);
