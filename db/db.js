381413c50c7677060977dc01cdb20ebc8fe78038
*Vaibhav <*vaibhavjagtap0925@gmail.com*>
2026-01-30 08:49:04 +0530

    node api db conncection railway

--- db/db.js
+++ db/db.js
@@ -1,21 +1,26 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
-dotenv.config();   
-const db = mysql.createConnection({
-host: process.env.DB_HOST,
-port: process.env.DB_PORT,
-user: process.env.DB_USER,
-password: process.env.DB_PASSWORD,
-database: process.env.DB_NAME,
+
+dotenv.config();
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+// ✅ Test connection using query
+db.query("SELECT 1", (err) => {
   if (err) {
-    console.log()
     console.error("❌ MySQL connection failed:", err.message);
-    return;
+  } else {
+    console.log("✅ MySQL Pool Connected");
   }
-  console.log("✅ MySQL Connected");
 });
 
 export default db;
